ddb3f4c0794188bb3d45a89114eac676070b9101
justinrivera47 <justinrivera47@gmail.com>
2022-02-12 19:18:36 -0500

    setting up the website

---
+++ my-photobooth-gatsby-website/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/justinrivera/Desktop/photobooth/my-photobooth-gatsby-website/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/justinrivera/Desktop/photobooth/my-photobooth-gatsby-website/src/pages/index.js"))
+}
+
